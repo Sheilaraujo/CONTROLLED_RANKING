04039f00add522088ed95526a4890b9e8672048e
Sheila Araujo <sheilla-araujo@hotmail.com>
2022-05-28 17:31:37 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :schedules, only: [:index, :new, :create, :destroy, :show]
-  resources :games, only: [:new, :create, :show]
+  resources :games, only: [:index, :new, :create, :show]
   resources :teams, only: [:index, :new, :create, :show]
 end
