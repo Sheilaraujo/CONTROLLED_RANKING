35c1c315364b0178fd7a112fe229ea80ac9ef65a
Vinicius Vinci <vvinci@protonmail.com>
2022-06-02 20:24:33 -0300

    user controller edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def show
     @user = current_user.id
   end
+
+  def edit
+    @user = current_user.id
+  end
+
+  def update
+    @user = current_user.id
+    @user.update!(user_params)
+    redirect_to current_user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :last_name, :email, :about_me, :difficulty)
+  end
 end
