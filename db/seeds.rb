28f387d8def94296be045298c27e9ac2a762e05d
Cintia Pizzatto <cintiapizzatto@hotmail.com>
2022-06-09 20:14:47 -0300

    sched-dia7b

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,28 @@
 # Game.create(result: "3x2", team_id: , date: "", place: "Rio de Janeiro")
 # Team.create(player_1: , player_2: , score: , game_id: )
 
-# User.create(password: "123456", email: "cd@g.com", name: "Carla",   last_name: "Dias",
-# about_me: "jogo pouco", difficulty: "A", user_score: 0)
-# User.create(password: "123456", email: "ml@g.com", name: "Marcelo", last_name: "Lima",
-# about_me: "só posso jogar aos sábados", difficulty: "A", user_score: 0)
-# User.create(password: "123456", email: "lg@g.com", name: "Luis",    last_name: "Gama",
-# about_me: "sou de Curitiba", difficulty: "B", user_score: 0)
-# User.create(password: "123456", email: "md@g.com", name: "Michele", last_name: "Dantas",
-# about_me: "jogo muito!", difficulty: "B", user_score: 0)
-# User.create(password: "123456", email: "dc@d.com", name: "Davi",    last_name: "Castro",
-# about_me: "jogo devagar", difficulty: "B", user_score: 0)
-# User.create(password: "123456", email: "lf@g",     name: "Lucas",   last_name: "Fernandes",
-# about_me: "jogo rápido", difficulty: "A", user_score: 0)
-# User.create(password: "123456", email: "vc@g.com", name: "Vanessa", last_name: "Camargo",
-# about_me: "sou do litoral de SP", difficulty: "B", user_score: 10)
-# User.create(password: "123456", email: "vd@g.com", name: "Vitor",   last_name: "Dias",
-# about_me: "sou de SP capital", difficulty: "A", user_score: 10)
+User.create(password: "123456", email: "cd@g.com", name: "Carla",   last_name: "Dias",
+about_me: "jogo pouco", difficulty: "A", user_score: 0)
+User.create(password: "123456", email: "ml@g.com", name: "Marcelo", last_name: "Lima",
+about_me: "só posso jogar aos sábados", difficulty: "A", user_score: 0)
+User.create(password: "123456", email: "lg@g.com", name: "Luis",    last_name: "Gama",
+about_me: "sou de Curitiba", difficulty: "B", user_score: 0)
+User.create(password: "123456", email: "md@g.com", name: "Michele", last_name: "Dantas",
+about_me: "jogo muito!", difficulty: "B", user_score: 0)
+User.create(password: "123456", email: "dc@g.com", name: "Davi",    last_name: "Castro",
+about_me: "jogo devagar", difficulty: "B", user_score: 0)
+User.create(password: "123456", email: "lf@g.com",     name: "Lucas",   last_name: "Fernandes",
+about_me: "jogo rápido", difficulty: "A", user_score: 0)
+User.create(password: "123456", email: "vc@g.com", name: "Vanessa", last_name: "Camargo",
+about_me: "sou do litoral de SP", difficulty: "B", user_score: 10)
+User.create(password: "123456", email: "vd@g.com", name: "Vitor",   last_name: "Dias",
+about_me: "sou de SP capital", difficulty: "A", user_score: 10)
 
-Schedule.create(date: "Wed, 08 Jun 2022", place: "Rio de Janeiro", user_id: 1, partner: {duo: 2, opponent_1: 3, opponent_2: 4})
-Schedule.create(date: "Wed, 08 Jun 2022", place: "São Paulo", user_id: 5, partner: {duo: 6, opponent_1: 7, opponent_2: 8})
+Schedule.create(date: "Wed, 01 Jul 2022", place: "Rio de Janeiro", user_id: 1, partner: {duo: 2, opponent_1: 3, opponent_2: 4})
+Schedule.create(date: "Wed, 02 Jul 2022", place: "São Paulo", user_id: 1, partner: {duo: 3, opponent_1: 0, opponent_2: 0})
+Schedule.create(date: "Wed, 03 Jul 2022", place: "Rio de Janeiro", user_id: 1, partner: {duo: 3, opponent_1: 5, opponent_2: 6})
+Schedule.create(date: "Wed, 04 Jul 2022", place: "São Paulo", user_id: 1, partner: {duo: 0, opponent_1: 3, opponent_2: 0})
+Schedule.create(date: "Wed, 05 Jul 2022", place: "Rio de Janeiro", user_id: 5, partner: {duo: 0, opponent_1: 1, opponent_2: 7})
+Schedule.create(date: "Wed, 06 Jul 2022", place: "São Paulo", user_id: 5, partner: {duo: 4, opponent_1: 3, opponent_2: 0})
+Schedule.create(date: "Wed, 07 Jul 2022", place: "Rio de Janeiro", user_id: 5, partner: {duo: 6, opponent_1: 5, opponent_2: 4})
+Schedule.create(date: "Wed, 08 Jul 2022", place: "São Paulo", user_id: 5, partner: {duo: 1, opponent_1: 7, opponent_2: 8})
