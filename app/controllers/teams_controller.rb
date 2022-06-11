54604049ac7de787efd10a3fb87a96e1ea4357e4
Cintia Pizzatto <cintiapizzatto@hotmail.com>
2022-06-11 09:05:06 -0300

    sched-dia9

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,19 +1,24 @@
 class TeamsController < ApplicationController
-    def show
-        @teams = Teams.find(params[:id])
-    end
+  def index
+    @teams = Team.all
+    @schedules = Schedule.all
+    @users = User.all
+  end
 
-    def new
-        @teams = Teams.new
-    end
+  def show
+    @teams = Teams.find(params[:id])
+  end
 
-    def create
-        @teams = teams.new(teams_params)
-        if @teams.save
-            redirect_to teams_path(@teams)
-        else
-            render :new
-        end
-    end
+  def new
+    @teams = Teams.new
+  end
 
+  def create
+    @teams = teams.new(teams_params)
+    if @teams.save
+      redirect_to teams_path(@teams)
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure__params, only: [:create]
 
-  # GET /resource/sign_in
+  # GET /resource/
   # def new
   #   super
   # end
 
-  # POST /resource/sign_in
+  # POST /resource/
   # def create
   #   super
   # end
