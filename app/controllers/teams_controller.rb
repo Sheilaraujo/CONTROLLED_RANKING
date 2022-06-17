ad35a95b2801cea6e5610fb6748421f953115e8c
Vinicius Vinci <vvinci@protonmail.com>
2022-06-16 23:33:42 -0300

    teams change

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,11 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!, only: :index
+
   def index
     @teams = Team.where(player_1: current_user.id)
+    if @teams.empty?
+      @teams = Team.where(player_2: current_user.id)
+    end
   end
 
   def show
@@ -9,26 +13,40 @@ class TeamsController < ApplicationController
   end
 
   def new
-    @team_home = Team.new
+    @team = Team.new
     @team_visitor = Team.new
-    @schedule = Schedule.find(params[:format])
-    @team_home.player_1 = @schedule.user_id
-    @team_home.player_2 = @schedule.partner["duo"]
+    @schedule = Schedule.find(params[:id])
+    @team.player_1 = @schedule.user_id
+    @team.player_2 = @schedule.partner["duo"]
   end
 
   def create
-    @team_home = Team.new
+    @team = Team.new
     @team_visitor = Team.new
-    @schedule = Schedule.find(params[:format])
-    @team_home.player_1 = @schedule.user_id
-    @team_home.player_2 = @schedule.partner["duo"]
-    @team_home.score = params[:score]
-    @team_home.schedule_id = params[:schedule_id]
-    if @teams.save
-      redirect_to teams_path(@teams)
+
+    @team.player_1 = params[:player_1].to_i
+    @team.player_2 = params[:player_2].to_i
+    @team.schedule_id = params[:schedule_id].to_i
+
+    @team_visitor.player_1 = params[:player_3].to_i.freeze
+    @team_visitor.player_2 = params[:player_4].to_i
+    @team_visitor.schedule_id = params[:schedule_id].to_i
+
+    @team.status = "confirmed"
+    @team_visitor.status = "confirmed"
+    byebug
+    if @team.save && @team_visitor.save
+      redirect_to teams_path
+      flash[:notice] = "Partida Confirmada!"
     else
       render :new
     end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:schedule_id, :player_1, :player_2, :player_3, :player_4)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_163143) do
+ActiveRecord::Schema.define(version: 2022_06_16_003800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,9 +65,7 @@ ActiveRecord::Schema.define(version: 2022_06_15_163143) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "schedule_id", null: false
-    t.bigint "game_id", null: false
     t.string "status", default: "standby", null: false
-    t.index ["game_id"], name: "index_teams_on_game_id"
     t.index ["schedule_id"], name: "index_teams_on_schedule_id"
   end
 
@@ -92,6 +90,5 @@ ActiveRecord::Schema.define(version: 2022_06_15_163143) do
   add_foreign_key "games", "teams"
   add_foreign_key "messages", "users"
   add_foreign_key "schedules", "users"
-  add_foreign_key "teams", "games"
   add_foreign_key "teams", "schedules"
 end
